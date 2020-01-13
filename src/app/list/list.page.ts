15e1f6f467f547795446d530dc1b82e735d66f71
Sami-Rocks <sowusuaboagye@gmail.com>
2020-01-13 09:24:00 +0000

    clean up

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
 import { PhotoService } from './../services/photo.service';
-
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-list',
@@ -11,38 +13,70 @@ import { PhotoService } from './../services/photo.service';
 
 export class ListPage implements AfterViewInit {
   
-  @ViewChild("image", {static:false}) imgid: ElementRef;
+  @ViewChild("image", {static: true}) imgid: ElementRef<HTMLImageElement>;
+  @ViewChild("canvas", {static: true}) canvas: ElementRef<HTMLCanvasElement>;
  
   predicted:any = [];
 
   photo_array:any = [];
   current:any;
   _image:string = "./../../assets/image1.jpg";
+  w_diff:number;
+  h_diff:number;
 
- 
+  private ctx: CanvasRenderingContext2D;
+  private loading;
+  
   constructor(
-    public photoService:PhotoService
+    public photoService:PhotoService,
+    public router:Router,
+    private loadingCtrl:LoadingController
   ){}
   
 
   ngAfterViewInit() {
-
-    this.photoService.loadSaved();
-    this.photo_array=this.photoService.photos;
-    this.current = this.photo_array[0];
-    this._image = this.current.data;
-
+    const img = this.imgid.nativeElement;
+    const canvas = this.canvas.nativeElement;
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+     
+    setTimeout(e => this.ctx.drawImage(img, 0, 0, img.width, img.height,   0, 0, canvas.width, canvas.height), 500);
+    // this.photoService.loadSaved();
+    // this.photo_array=this.photoService.photos;
+    // this.current = this.photo_array[0];
+    // this._image = this.current.data;
+    
   }
 
   async predict(){
       
     const img = this.imgid.nativeElement;
-
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...',
+      duration: 20000
+    });
+    await loading.present();
     const model = await cocoSsd.load();
     console.log("model loaded");
+    await loading.dismiss();
     const predictions = await model.detect(img);
     console.log(predictions);
     this.predicted = predictions;
+    /* this.predicted.forEach(element => {
+      this.draw(Math.round(element.bbox[0]), Math.round(element.bbox[1]), Math.round(element.bbox[2]), Math.round(element.bbox[3]), 'red', element.class);
+      //console.log(element.bbox[0])
+    }); */
+    
   }
  
+  goBack(){
+    this.router.navigateByUrl('home');
+  }
+
+  draw(x:number, y:number, width:number, height:number, color:string, className:string){
+    this.ctx.strokeStyle = color;
+    this.ctx.lineWidth = 2;
+    this.ctx.fillText(className, x, y);
+    this.ctx.scale(.75, .85);
+    this.ctx.strokeRect(x, y, width, height);
+  }
 }
